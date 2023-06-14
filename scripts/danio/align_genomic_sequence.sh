e3661db1decce0176bbd175c5f06bba10a22665e
Jonathan Wells <jnw72@cornell.edu>
2023-06-14 17:35:54 -0400

    Fix  bugs in align_genomic_sequence.sh

--- scripts/danio/align_genomic_sequence.sh
+++ scripts/danio/align_genomic_sequence.sh
@@ -9,16 +9,15 @@ declare -A GENOMES=(
     ["GCA_018400075.1_ASM1840007v1_genomic.fna"]="T5D"   # T5D
     ["GCA_903798165.1_fDreCBz1.1_genomic.fna"]="CB"   # Cooch-Behar
     ["GCA_903798175.1_fDreNAz1.1_genomic.fna"]="NA"   # Nadia
-    ["GCF_000002035.6_GRCz11_genomic.fna"]="REF"   # Reference
 )
 
 for genome in ${!GENOMES[@]}; do
-    strain=$GENOMES[$genome]
+    strain=${GENOMES[$genome]}
     echo "aligning ${strain} to reference"
-    minimap2 \
+    /programs/minimap2-2.24/minimap2 \
         -ax asm5 \
         -t 20 \
-        --eqx "${GENOMEDIR}/GCF_000002035.6_GRCz11_genomic.fna" \
+        --eqx "${GENOMEDIR}/GCF_000002035.6_GRCz11_genomic.nonalt.fna" \
         "${GENOMEDIR}/${genome}" |
     samtools sort -O BAM - > "${DATADIR}/Ref_${strain}_alignment.bam"
     samtools index "${DATADIR}/Ref_${strain}_alignment.bam"
